78339f84468daca5d8118fbb70a94b7d2c3686bf
Aymeric Le Feyer <lefeyer.aymeric@gmail.com>
2019-10-21 18:17:14 +0200

    Fibo

---
+++ tp1/suiteReccurente.py
@@ -0,0 +1,29 @@
+import time
+
+# Fibonacci
+
+
+def fiboRec(n):
+    if n <= 1:
+        return 1
+    return fiboRec(n - 1) + fiboRec(n - 2)
+
+
+def fiboIte(n):
+    tab = [1, 1]
+    for i in range (2, n+1):
+        tab.append(tab[i - 1] + tab[i - 2])
+    return tab[n]
+
+
+a = 35
+
+now = time.time()
+fiboRec(a)
+new = time.time() - now
+print(new)
+
+now = time.time()
+fiboIte(a)
+new = time.time() - now
+print(new)
